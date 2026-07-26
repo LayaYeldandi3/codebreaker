8dd07e39d0d48847c7df3c71c0b0191afa1f7a5c
LayaYeldandi3 <23wh1a12b3@bvrithyderabad.edu.in>
2026-07-26 14:08:18 +0530

    Fix controller package structure

--- backend/backend/src/main/java/com/codebreaker/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/codebreaker/backend/BackendApplication.java
@@ -1,13 +1,8 @@
-package com.codebreaker.backend;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+package com.codebreaker.backend; // Check package name
 
 @SpringBootApplication
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
